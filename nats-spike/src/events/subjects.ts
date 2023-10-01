9d459338f90231ee39c9df0b009b45b2747232b2
Jerome Gonzalvo <gonzalvo.jerome@gmail.com>
2023-10-01 10:50:25 +0800

    removed experiment on nats

--- nats-spike/src/events/subjects.ts
+++
@@ -1,4 +0,0 @@
-export enum Subjects {
-  TICKET_CREATED = "ticket:created",
-  ORDER_UPDATED = "order:updated"
-}
--- nats-spike/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-publisher";
-import { Subjects } from "./subjects";
-import { TicketCreatedEvent } from "./ticket-created-event";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  subject: Subjects.TICKET_CREATED = Subjects.TICKET_CREATED;
-}
